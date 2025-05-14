5dae5107014dc89c425fad7ff598d49baeb041ad
Sylvia <lyd.dontbeafraid@gmail.com>
2025-05-14 09:22:13 +0000

    test

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,12 +1,13 @@
-select date_date,
-COUNT(orders_id) AS nb_transactions,
-ROUND(SUM(revenue),2) AS total_revenue,
-ROUND(SUM(revenue)/COUNT(orders_id)) AS average_basket,
-ROUND(SUM(operational_margin),2) AS operational_margin,
-ROUND(SUM(purchase_cost),2) AS purchase_cost,
-ROUND(SUM(shipping_fee),2) AS shipping_fee,
-ROUND(SUM(logcost),2) AS logcost,
-ROUND(SUM(quantity),2) AS total_quantity_of_products_sold
-from {{ref("int_orders_operational")}}
+select
+    date_date,
+    count(orders_id) as nb_transactions,
+    round(sum(revenue), 2) as total_revenue,
+    round(sum(revenue) / count(orders_id)) as average_basket,
+    round(sum(operational_margin), 2) as operational_margin,
+    round(sum(purchase_cost), 2) as purchase_cost,
+    round(sum(shipping_fee), 2) as shipping_fee,
+    round(sum(logcost), 2) as logcost,
+    round(sum(quantity), 2) as total_quantity_of_products_sold
+from {{ ref("int_orders_operational") }}
 group by date_date
 order by date_date desc
